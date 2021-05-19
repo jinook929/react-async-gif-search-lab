508f351827ffa90827ce425e21a850ad77f0e7ea
jinook929 <jinook929@gmail.com>
2021-05-18 22:41:58 -0400

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react'
+import GifSearch from '../components/GifSearch'
 import GifList from '../components/GifList'
 
-// const BASE_URL = `https://api.giphy.com/v1/gifs/search?q=`
-// const API_KEY = `&api_key=4UFVclyms4yPlI0NsFMFPAoB0JCtZIAe&rating=g`
+const BASE_URL = `https://api.giphy.com/v1/gifs/search?q=`
+const API_KEY = `&api_key=4UFVclyms4yPlI0NsFMFPAoB0JCtZIAe&rating=g`
 
 export class GifListContainer extends Component {
-  // state = {
-  //   gifs: this.props.gifs
-  // }
-  // fetchGifs = () => {
-  //   fetch(`${BASE_URL}${this.props.searchTerm}${API_KEY}`).then(res => res.json())
-  //   .then(data => {
-  //     const threeGifs = []
-  //     for(let i = 0; i < 3; i++) {
-  //       threeGifs.push(data.data[i])
-  //     }
-  //     this.setState(state => ({gifs: threeGifs}))
-  //   })    
-  // }
-  // componentDidMount() {
-  //   console.log("GifListContainer componentDidMount")
-  //   // this.fetchGifs()
-  //   // this.props.fetchGifs()
-  // }
+  state = {
+    gifs: []
+  }
+  fetchGifs = (keyword = "keyword") => {
+    fetch(`${BASE_URL}${keyword}${API_KEY}`).then(res => res.json())
+    .then(data => {
+      const threeGifs = []
+      for(let i = 0; i < 3; i++) {
+        threeGifs.push(data.data[i])
+      }
+      this.setState(state => ({gifs: threeGifs}))
+    })    
+  }
+  handleSubmitSearchTerm = (keyword) => {
+    this.fetchGifs(keyword)
+  }
+  componentDidMount() {
+    this.fetchGifs()
+  }
   render() {
     return (
       <div className="GifListContainer">
         <ul>
-          <GifList gifs={this.props.gifs} />
+          <GifSearch handleSubmitSearchTerm={this.handleSubmitSearchTerm} />
+          <GifList gifs={this.state.gifs} />
         </ul>
       </div>
     )
